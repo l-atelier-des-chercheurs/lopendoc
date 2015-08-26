10fc6a17f566b024b0cbdc940f74956dcba50daa
Louis Eveillard <louis.eveillard@gmail.com>
2015-08-26 09:35:17 +0200

    bugfix oublie d'un php dans <?php

--- WP/taxonomy-projets.php
+++ WP/taxonomy-projets.php
@@ -3,45 +3,6 @@
 	$tax = get_query_var( 'taxonomy' );
 	$term = get_query_var( 'term' );
 
-/*
-	// si requête POST
-  if ('POST' === $_SERVER['REQUEST_METHOD']
-      && ! empty($_POST['action'])
-      && isset($_POST['post_id'])
-  )  {
-
-    $post = array();
-    $post['ID'] = $_POST['post_id'];
-
-	  // si changer la visibilité du post :
-	  if ( $_POST['action'] === 'update_post_visibility' ) {
-
-      switch ($_POST['visibility']) {
-          case 'private':
-              $post['post_status'] = 'private';
-              break;
-          case 'publish':
-              $post['post_status'] = 'publish';
-              $post['post_password'] = '';
-              break;
-          case 'password':
-              $post['post_status'] = 'publish';
-              $post['post_password'] = $_POST['post_password'];
-              break;
-      }
-      wp_update_post($post);
-  	} else
-	  if ( $_POST['action'] === 'remove_post' ) {
-			wp_trash_post( $post['ID'] );
-  	} else
-	  if ( $_POST['action'] === 'set_taxonomy' ) {
-			wp_set_object_terms( $post['ID'], array($term), 'projets');
-  	}
-
-  }
-*/
-
-
 	$args = array(
     'tax_query'         => array(
         'relation'      => 'AND',
@@ -67,7 +28,9 @@
 	if ( $wp_query->have_posts() ) {
 
 			// The Loop
-			while ($wp_query->have_posts()) : $wp_query->the_post();
+			while( $wp_query->have_posts() ) {
+
+				$wp_query->the_post();
 
 			  $ID = get_the_ID();
 			  $status = get_post_status($ID);
@@ -76,10 +39,10 @@
 
 				<div class="descriptionContainer">
 
-					<section class="post" data-post="<?php $ID; ?>" <?php post_class(); ?> data-status="<?php echo $status; ?>" data-id="<?php echo $ID ?>" data-action="<?php echo esc_url(home_url('')); ?>/edit-page" data-singleurl="<?php echo esc_url( get_permalink() ); ?>">
+					<section class="post" data-post="<?php echo $ID; ?>" <?php post_class(); ?> data-status="<?php echo $status; ?>" data-id="<?php echo $ID ?>" data-action="<?php echo esc_url(home_url('')); ?>/edit-page" data-singleurl="<?php echo esc_url( get_permalink() ); ?>">
 
 					<?php
-						if ( is_user_logged_in() ) {
+					if ( is_user_logged_in() ) {
 							?>
 
 								<?php get_template_part('templates/entry-button-right'); ?>
@@ -117,8 +80,8 @@
 
 				</div>
 
-			<?
-			endwhile;
+		<?php
+			}
 
 	} else {
 		?>
@@ -133,7 +96,6 @@
 	wp_reset_query();
 ?>
 
-
 	<article class="projetContainer taxProj" data-taxonomy="<?php echo $tax; ?>" data-term="<?php echo $term; ?>">
 		<div class="colTitle">
 			<h2 class="entry-title">
@@ -141,24 +103,21 @@
 			</h2>
 			<?php
 			  if ( is_user_logged_in() ) {
-					      $mailToContribute =  get_option( "mail_addressTC" );
-					      if( !empty($mailToContribute) ) {
-									echo "<h3 class='instructions'>";
-						      	$mailToContribute = str_replace("leprojet", $term, $mailToContribute);
-						    		_e("To contribute, send an email to ", 'opendoc');
-										echo "<a target='_blank' href='mailto:" . $mailToContribute . "'>" . $mailToContribute . "</a>";
-						    		_e(", or click on <strong>Add a post</strong>.", 'opendoc');
-						    	echo "</h3>";
-						    }
-
-
+		      $mailToContribute =  get_option( "mail_addressTC" );
+		      if( !empty($mailToContribute) ) {
+						echo "<h3 class='instructions'>";
+			      	$mailToContribute = str_replace("leprojet", $term, $mailToContribute);
+			    		_e("To contribute, send an email to ", 'opendoc');
+							echo "<a target='_blank' href='mailto:" . $mailToContribute . "'>" . $mailToContribute . "</a>";
+			    		_e(", or click on <strong>Add a post</strong>.", 'opendoc');
+			    	echo "</h3>";
+			    }
 				}
 			?>
 
 		</div>
 <?php
   if ( is_user_logged_in() ) {
-
 			?>
 
 		<div class="topIcons">
@@ -190,7 +149,8 @@
 	if ( $new_wp_query->have_posts() ) {
 
 		// The Loop
-		while ($new_wp_query->have_posts()) : $new_wp_query->the_post();
+		while ($new_wp_query->have_posts()) {
+			$new_wp_query->the_post();
 
 		 	$featured =  has_tag('featured');
 	 		if( $featured == true ) {
@@ -198,21 +158,17 @@
 			}
 
 			get_template_part('templates/content', 'carte');
+		}
 
-
-		endwhile;
-
-		//echo 'lastPosttime : ' . $lastPosttime;
-
-		?>
-		<?php if ($new_wp_query->max_num_pages > 1) : ?>
+		if ($new_wp_query->max_num_pages > 1) { ?>
 		  <nav class="post-nav">
 		    <ul class="pager">
 		      <li class="previous"><?php next_posts_link(__('&larr; Older posts', 'roots')); ?></li>
 		      <li class="next"><?php previous_posts_link(__('Newer posts &rarr;', 'roots')); ?></li>
 		    </ul>
 		  </nav>
-		<?php endif;
+		<?php
+		}
 
 	}
 
